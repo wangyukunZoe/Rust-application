68b10565456e2ddc7d7253b538db2136724ebd8a
wangyukunZoe <wangyukun20011027@icloud.com>
2026-01-27 01:40:41 +0800

    use_keyword
    use_keyword

---
+++ module_sys/host_2/src/main.rs
@@ -0,0 +1,21 @@
+use rand::Rng;
+use std::collections::HashMap;
+// use std::cmp::Ordering;
+
+// use std::io::Write;
+// use std::io;
+
+use std::io::{self, Write};
+
+//谨慎使用全部引用
+//通配符引用一般用在测试时常用。方便测试不同的测试用例
+use std::collections::*;
+
+
+// use std::{cmp::Ordering, io};    //普通写法
+// 可使用嵌套路径在同一行完成库的导入
+
+//有些库因为非常常用，编译器🧬器已经帮我们做了预导入处理
+fn main() {
+    println!("Hello, world!");
+}
